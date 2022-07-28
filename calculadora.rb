2694dc53cb63d2a35723d6254c198d5ec01200e0
xSergioLimax <sergiolimajr05@gmail.com>
2022-07-28 01:35:12 -0300

    implementação da função subtração

--- calculadora.rb
+++ calculadora.rb
@@ -5,5 +5,10 @@ class Calculadora
     return soma_calculada.round(1)
     end
 
+    def subtrai(a, b)
+        subtracao_calculada= a - b
+    return subtracao_calculada.round(1)
+    end
+
     
 end
