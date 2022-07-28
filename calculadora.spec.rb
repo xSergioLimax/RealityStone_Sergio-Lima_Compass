f391fa4e09ffbc0e73b8c793a50a9cd2851b0253
xSergioLimax <sergiolimajr05@gmail.com>
2022-07-28 02:55:52 -0300

    adição dos testes feitos para multiplicação

--- calculadora.spec.rb
+++ calculadora.spec.rb
@@ -4,6 +4,8 @@ require_relative '../src/calculadora'
 describe ('Calculadora') do
     calculadora = Calculadora.new
 
+    # Testes relacionados a adição
+    
     it ('Deve somar dois inteiros positivos') do
         expect(calculadora.soma(4, 5)).to eq 9
     end
@@ -86,4 +88,45 @@ describe ('Calculadora') do
         expect(calculadora.subtrai(0, 9)).to eq -9
     end
 
-end
+    # Apartir daqui testes relacionados a multiplicação
+
+    it ('Deve multiplicar dois inteiros positivos') do
+        expect(calculadora.multiplica(4, 5)).to eq 20
+    end
+
+    it ('Deve multiplicar inteiros positivos com inteiros negativos') do
+        expect(calculadora.multiplica(5, -8)).to eq -40
+    end
+
+    it ('Deve multiplicar inteiros positivos com decimais positivos') do
+        expect(calculadora.multiplica(8, 5.7)).to eq 45.6
+    end
+
+    it ('Deve multiplicar inteiros positivos com decimais negativos') do
+        expect(calculadora.multiplica(8, -3.6)).to eq -28.8
+    end
+
+    it ('Deve multiplicar dois decimais positivos') do
+        expect(calculadora.multiplica(1.6, 6.3)).to eq 10.1
+    end
+
+    it ('Deve multiplicar dois decimais negativos') do
+        expect(calculadora.multiplica(-4.2, -5.1)).to eq 21.4
+    end
+
+    it ('Deve multiplicar dois inteiros negativos') do
+        expect(calculadora.multiplica(-9, -10)).to eq 90
+    end
+
+    it ('Deve multiplicar inteiros negativos com decimais positivos') do
+        expect(calculadora.multiplica(-8, 8.4)).to eq -67.2
+    end
+
+    it ('Deve multiplicar inteiros negativos com decimais negativos') do
+        expect(calculadora.multiplica(-2, -5.4)).to eq 10.8
+    end
+
+    it ('Deve multiplicar qualquer número com 0') do
+        expect(calculadora.multiplica(0, 9)).to eq 0
+    end
+end   
